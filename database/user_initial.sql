1d5ef3b07477e2f295878f91f94dfe1fbbff2081
ThisIsMatin <callmethematt@gmail.com>
2021-06-18 11:26:06 +0430

    Create TSQL files

---
+++ database/user_initial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user` (
+    `id` bigint(10) NOT NULL,
+    `step` varchar(50) NOT NULL,
+    `voicename` char(200) DEFAULT NULL,
+    `voicemode` char(50) DEFAULT NULL,
+    `sendvoice` int(1) NOT NULL DEFAULT 0,
+    `voiceedit` char(50) DEFAULT NULL,
+    `sortby` char(100) NOT NULL DEFAULT 'newest'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `user`
+    ADD PRIMARY KEY (`id`);
---
+++ database/voices_initial.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `voices` (
+    `id` int(10) NOT NULL,
+    `unique_id` char(250) NOT NULL,
+    `accepted` char(200) DEFAULT '0',
+    `name` char(200) NOT NULL,
+    `url` char(200) NOT NULL,
+    `sender` char(200) NOT NULL,
+    `messageid` char(200) NOT NULL,
+    `mode` char(200) NOT NULL,
+    `usecount` int(50) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `voices`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `voices`
+    MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
+    COMMIT;
