c8d2084c14c594fb45d4d81a728c40422f817886
Matin Najafi <matin.najafi@gmail.com>
2021-06-19 10:11:46 +0430

    Create autobackup.php

---
+++ autobackup.php
@@ -0,0 +1,145 @@
+<?php
+
+$CONFIG = json_decode(file_get_contents('config.json'), true);
+
+define('API_KEY', $CONFIG['TOKEN']);
+
+//-----------------------------------------
+
+function Bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+       return $res;
+    }
+}
+
+$dbhost = 'localhost';
+$dbuser = $CONFIG['DATABASE']['USERNAME'];
+$dbpass = $CONFIG['DATABASE']['PASSWORD'];
+$dbname = $CONFIG['DATABASE']['DBNAME'];
+$tables = '*';
+
+//Call the core function
+backup_tables($dbhost, $dbuser, $dbpass, $dbname, $tables);
+
+//Core function
+function backup_tables($host, $user, $pass, $dbname, $tables = '*') {
+    $link = mysqli_connect($host,$user,$pass, $dbname);
+
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit;
+    }
+
+    mysqli_query($link, "SET NAMES 'utf8'");
+
+    //get all of the tables
+    if($tables == '*')
+    {
+        $tables = array();
+        $result = mysqli_query($link, 'SHOW TABLES');
+        while($row = mysqli_fetch_row($result))
+        {
+            $tables[] = $row[0];
+        }
+    }
+    else
+    {
+        $tables = is_array($tables) ? $tables : explode(',',$tables);
+    }
+
+    $return = '';
+    //cycle through
+    foreach($tables as $table)
+    {
+        $result = mysqli_query($link, 'SELECT * FROM '.$table);
+        $num_fields = mysqli_num_fields($result);
+        $num_rows = mysqli_num_rows($result);
+
+        $return.= 'DROP TABLE IF EXISTS '.$table.';';
+        $row2 = mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE '.$table));
+        $return.= "\n\n".$row2[1].";\n\n";
+        $counter = 1;
+
+        //Over tables
+        for ($i = 0; $i < $num_fields; $i++) 
+        {   //Over rows
+            while($row = mysqli_fetch_row($result))
+            {   
+                if($counter == 1){
+                    $return.= 'INSERT INTO '.$table.' VALUES(';
+                } else{
+                    $return.= '(';
+                }
+
+                //Over fields
+                for($j=0; $j<$num_fields; $j++) 
+                {
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = str_replace("\n","\\n",$row[$j]);
+                    if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                    if ($j<($num_fields-1)) { $return.= ','; }
+                }
+
+                if($num_rows == $counter){
+                    $return.= ");\n";
+                } else{
+                    $return.= "),\n";
+                }
+                ++$counter;
+            }
+        }
+        $return.="\n\n\n";
+    }
+
+    //save file
+    $fileName = 'ohpesar-dbbackup-'.time().'-'.(md5(implode(',',$tables))).'.zip';
+    $handle = fopen($fileName,'w+');
+    fwrite($handle,$return);
+    if(fclose($handle)){
+        $document = 'https://server-me.ir/bots/theohpesar/'.$fileName;
+        $db = $link;
+        $all_voices = $unaccepted_voice = $accepted_voice = $all_users = $private_voices = 0;
+    
+        $all_users = mysqli_num_rows(mysqli_query($db, "SELECT * FROM `user`"));
+        
+        $query = mysqli_query($db, "SELECT * FROM `voices`");
+        $all_voices = mysqli_num_rows($query);
+    
+        for ($i=0; $i < $all_voices; $i++) {
+            $thevoice = mysqli_fetch_assoc($query);
+            if($thevoice['mode'] == 'private'){
+                $private_voices++;
+            }else{
+                if($thevoice['accepted']){
+                    $accepted_voice++;
+                }else{
+                    $unaccepted_voice++;
+                }
+            }
+        }
+        
+        
+        $caption = "Backup from OhPesar Database
+        
+Bot users : $all_users
+
+Bot Voices : $all_voices
+Private Voices : $private_voices
+Accepted Voices : $accepted_voice
+Unaccepted Voices : $unaccepted_voice";
+        Bot('SendDocument',['chat_id'=>'-1001491735326', 'document'=>$document,'caption'=>$caption]);
+        sleep(1);
+        unlink($fileName);
+        exit;
+    }
+}
