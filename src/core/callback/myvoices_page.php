8b9ff091118d6e428983d1e9bc4323bb7975fe97
Matin Najafi <matin.najafi@gmail.com>
2021-07-07 13:28:31 +0430

    Update myvoices_page.php

--- src/core/callback/myvoices_page.php
+++ src/core/callback/myvoices_page.php
@@ -42,8 +42,9 @@ if($callback_query){
                 }
             }
             if($user_voice_info['mode'] == 'public'){ $voiceemoji = '🎤'; }else{ $voiceemoji = '🔐'; }
+            $switchquery = ['byname'=>$user_voice_info['name'], 'byid'=>'-id '.$user_voice_info['id']][$user['sendvoiceaction']];
             $MyVoicesKey[] = [
-                ['text'=>$voiceemoji.' '.$user_voice_info['name'], 'switch_inline_query'=>$user_voice_info['name']],
+                ['text'=>$voiceemoji.' '.$user_voice_info['name'], 'switch_inline_query'=>$switchquery],
                 ['text'=>'⚙️ تنظیمات ویس', 'callback_data'=>'voicesettings__'.$user_voice_info['unique_id'].'__'.$pagenum],
             ];
         }
