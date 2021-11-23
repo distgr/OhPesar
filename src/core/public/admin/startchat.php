13f0c7abfb6a2a57730dd72b836a1ccf01785128
Matin Najafi <matin.najafi@gmail.com>
2021-11-23 09:07:20 +0330

    Update start-chat systems

--- src/core/public/admin/startchat.php
+++ src/core/public/admin/startchat.php
@@ -12,20 +12,44 @@ if($text == '💬 شروع گفتوگو با کاربر' && in_array($from_id, $
 }
 
 elseif($text && $user['step'] == 'startchat1'){
+    $user_data = 'nonex';
+    $getted_start = false;
+    if(!is_numeric($text)){
+        SendMessage($chat_id, 'درحال دریافت اطلاعات کاربر از طریق یوزرنیم... این عمل ممکن است کمی طول بکشد');
+        $user_data = file_get_contents($CONFIG['SERVERURL'].'userapi.php?id='.$text);
+        $getted_start = true;
+        if(strpos(strtolower($user_data), strtolower($text)) !== false){
+            $user_data_decode = json_decode($user_data, true);
+            $text = $user_data_decode['result']['userid'];
+        }
+    }
     $chatuser_info = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM `user` WHERE `id` = '{$text}' LIMIT 1"));
-    if(!$chatuser_info){
+    if(!$chatuser_info or !is_numeric($text)){
         SendMessage($chat_id, 'چنین کاربری در دیتابیس «اوه پسر» یافت نشد !');
         mysqli_close($db);
         exit();
     }
-    Bot('sendMessage',[
-        'chat_id'=>$chat_id,
-        'text'=>"گفتوگو با این کاربر در کانال OhPesar Contact آغاز شد.",
-        'reply_markup'=>json_encode(['keyboard'=>$adminpanel ,'resize_keyboard'=>true])
-    ]);
+    if($user_data == 'nonex'){
+        Bot('sendMessage',[
+            'chat_id'=>$chat_id,
+            'text'=>"درحال دریافت اطلاعات این شخص، لطفا کمی صبر کنید...",
+            'reply_markup'=>json_encode(['keyboard'=>$adminpanel ,'resize_keyboard'=>true])
+        ]);
+    }
     
     $ContactMsgBtn = [];
-    $ContactMsgBtn[] = [['text'=>'👤 کاربر ناشناس', 'callback_data'=>'nothing']];
+    if($user_data == 'nonex') $user_data = file_get_contents($CONFIG['SERVERURL'].'userapi.php?id='.$text);
+    if(strpos($user_data, $text) !== false){
+        if(!$getted_start){
+            $user_data_decode = json_decode($user_data, true);
+        }
+        $ContactMsgBtn[] = [['text'=>'👤 '.$user_data_decode['result']['name'], 'callback_data'=>'nothing']];
+        if($user_data_decode['result']['username'] !== '@'){
+            $ContactMsgBtn[] = [['text'=>'🆔 '.$user_data_decode['result']['username'], 'url'=>'https://t.me/'.str_replace('@', '', $user_data_decode['result']['username'])]];
+        }
+    }else{
+        $ContactMsgBtn[] = [['text'=>'👤 کاربر ناشناس', 'callback_data'=>'nothing']];
+    }
     $ContactMsgBtn[] = [['text'=>'☑️ '.$text, 'callback_data'=>'nothing']];
     
     Bot('sendMessage',[
@@ -34,6 +58,11 @@ elseif($text && $user['step'] == 'startchat1'){
         'reply_markup'=>json_encode([
             'inline_keyboard'=>$ContactMsgBtn
         ])
+    ]);
+    Bot('sendMessage',[
+        'chat_id'=>$chat_id,
+        'text'=>"گفتوگو با این کاربر در کانال OhPesar Contact آغاز شد.",
+        'reply_markup'=>json_encode(['keyboard'=>$adminpanel ,'resize_keyboard'=>true])
     ]);
 
     $db->query("UPDATE `user` SET `step` = 'none' WHERE `id` = '{$from_id}' LIMIT 1");
